7c59ff2f8fac70d2831d0f245305ec5850c051db
ritik goyal <ritik.goyal987@gmail.com>
2019-07-23 09:46:15 +0530

    adding login page and name with messages

---
+++ public/js/chat.js
@@ -0,0 +1,36 @@
+
+var socket = io();
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+var user;
+  function getName(){
+    user=localStorage.getItem('name');
+    
+  }
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  if(message.from===user)
+  message.from="You";
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  var msg=jQuery('[name=message]').val();
+  
+  document.getElementById('message-form')['message'].value="";
+  socket.emit('createMessage', {
+    from: user,
+    text: msg
+  }, function () {
+
+  });
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,7 @@
-var socket = io();
 
-socket.on('connect', function () {
-  console.log('Connected to server');
-});
-
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
-});
-
-socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
-
-  jQuery('#messages').append(li);
-});
-
-jQuery('#message-form').on('submit', function (e) {
-  e.preventDefault();
-  var msg=jQuery('[name=message]').val();
-  document.getElementById('message-form')['message'].value="";
-  socket.emit('createMessage', {
-    from: 'User',
-    text: msg
-  }, function () {
-
-  });
-});
+function chat(){
+  var name=document.getElementById('form-name').value;
+  window.localStorage.setItem('name',name)
+  console.log(name);
+  return true;
+}
